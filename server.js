83164f2fdb6504d983033c2f81630ae6c6a8ea24
Anshuman Shukla <anshumanshukla721@gmail.com>
2025-10-07 16:12:27 +0530

    Added dotenv config

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 
 // Middleware
